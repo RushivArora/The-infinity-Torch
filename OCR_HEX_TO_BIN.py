8be6bc14280aab716e5a2a32060376738a6d594a
RushivArora <44711303+RushivArora@users.noreply.github.com>
2018-11-03 10:20:05 -0400

    Create OCR_HEX_TO_BIN.py

---
+++ OCR_HEX_TO_BIN.py
@@ -0,0 +1,86 @@
+import argparse
+import base64
+import picamera
+import json
+
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
+from google.cloud import pubsub_v1
+from google.cloud import storage
+from google.cloud import translate
+from google.cloud import vision
+
+path  = /home/pi/desktop/image.jpg
+
+def detect_handwritten_ocr(path):
+    """Detects handwritten characters in a local image.
+
+    Args:
+    path: The path to the local file.
+    """
+    from google.cloud import vision_v1p3beta1 as vision
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    # Language hint codes for handwritten OCR:
+    # en-t-i0-handwrit, mul-Latn-t-i0-handwrit
+    # Note: Use only one language hint code per request for handwritten OCR.
+    image_context = vision.types.ImageContext(
+        language_hints=['en-t-i0-handwrit'])
+
+    response = client.document_text_detection(image=image,
+                                              image_context=image_context)
+
+    print('Full Text: {}'.format(response.full_text_annotation.text))
+    for page in response.full_text_annotation.pages:
+        for block in page.blocks:
+            print('\nBlock confidence: {}\n'.format(block.confidence))
+
+            for paragraph in block.paragraphs:
+                print('Paragraph confidence: {}'.format(
+                    paragraph.confidence))
+
+                for word in paragraph.words:
+                    word_text = ''.join([
+                        symbol.text for symbol in word.symbols
+                    ])
+                    print('Word text: {} (confidence: {})'.format(
+                        word_text, word.confidence))
+
+                    for symbol in word.symbols:
+                        print('\tSymbol: {} (confidence: {})'.format(
+                            symbol.text, symbol.confidence))
+
+                        
+
+def detect_text_Bin_to_Hex(path):
+    """Detects text in the file."""
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+    """
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+    """
+    print(bin(int(text,16)))
+
+def main():
+    detect_handwritten_ocr(path)
+    detect_text(path)
